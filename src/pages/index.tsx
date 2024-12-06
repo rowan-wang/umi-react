e168d1f451883b65b62aead04375e50bbebc148a
汪伟 <wang.wei1@xyb2b.com>
2024-12-06 15:41:24 +0800

    fix: 增加调试代码

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { Table, Tag, Space, Card, Form, Input, Button } from 'antd';
 const Home: React.FC = () => {
+  console.log('测试代码', 1111)
+  const [data, setData] = useState('123')
+
   return (
     <Card title= {
       <Form layout="inline" className="searchform">
         <Form.Item label="文章详情" name="fclientCode">
           <Input />
         </Form.Item>
+        {data}
         <Form.Item >
           <Space>
             <Button type="primary">搜索</Button>
